e419df7b5220a0e1002c5d0cb94fa8b8c34f8c29
Joel Samson <joel.samson@hotmail.com>
2025-01-19 00:49:19 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,43 @@
 const express = require('express');
 const app = express();
-const Liquid = require('liquidjs');
+// Destructure the `Liquid` class instead of using it as a default function
+const { Liquid } = require('liquidjs');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const flash = require('express-flash');
 const cookieParser = require('cookie-parser');
 const logger = require('./src/logger');
 
-let engine = Liquid({
-    root: __dirname,
-    extname: '.liquid'
+// Instantiate the LiquidJS engine
+const engine = new Liquid({
+  root: __dirname, // or wherever your Liquid templates live
+  extname: '.liquid'
 });
 
+// Hook the Liquid engine into Express
 app.engine('liquid', engine.express());
 app.set('views', ['./views']);
 app.set('view engine', 'liquid');
 
+// Set up session store
 const sessionStore = new session.MemoryStore;
 
 app.use(session({
-    cookie: { maxAge: 60000 },
-    store: sessionStore,
-    saveUninitialized: true,
-    resave: 'true',
-    secret: 'secret'
+  cookie: { maxAge: 60000 },
+  store: sessionStore,
+  saveUninitialized: true,
+  resave: 'true',
+  secret: 'secret'
 }));
 app.use(cookieParser('secret'));
 app.use(flash());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+// Routes
 app.use(require('./routes'));
+
+// Serve static assets (Bootstrap, jQuery, etc.)
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js'));
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist'));
 app.use('/js', express.static(__dirname + '/node_modules/bootstrap-select/dist/js'));
@@ -38,5 +46,5 @@ app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css'));
 app.use('/css', express.static(__dirname + '/node_modules/bootstrap-select/dist/css'));
 
 app.listen(4568, function () {
-    logger.info('HelloHue running: http://localhost:4568');
+  logger.info('HelloHue running: http://localhost:4568');
 });
